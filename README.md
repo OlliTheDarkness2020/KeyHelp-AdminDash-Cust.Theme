ce70d09b953d549965bc304d4d970222b77355ce
Olli <67396171+OlliTheDarkness2020@users.noreply.github.com>
2021-03-19 03:25:48 +0100

    First Release (Ver. 21.0.1)
    Erste Veröffentlichung

--- README.md
+++ README.md
@@ -1,7 +1,28 @@
-KeyHelp-AdminDash-Cust.Theme
-KeyHelp Admin Dash
+<u> <b> KeyHelp Custom Admin Dash </b> </u>
 
-ACHTUNG: Das Theme für die Version 20+ findet Ihr unter: https://github.com/OlliTheDarkness2020/KeyHelp-AdminDash-Cust.Theme-OLD-
+<b> ACHTUNG: Das Theme für die Version 20+ findet Ihr unter: https://github.com/OlliTheDarkness2020/KeyHelp-AdminDash-Cust.Theme-OLD- </b>
 
-KeyHelp Admin Dash Repo für die kommende 21.0
+<hr>
 
+<h2> <b> <u> (Release-Version) Änderungen 19.03.2021 (Version 21.0.1) </u> </b> </h2>
+
+<b> Dash Panel </b>
+<ul>
+<li> Anpassung für KeyHelp 21 </li>
+
+<li> Entfernung der Updateanzeige des Custom Dash </li>
+
+<li> Entfernung der Notfallabschaltung (Emergency Lock) </li>
+
+<li> JavaScript Funktionen des Themes abgetrennt </li>
+
+<li> Kleines Bugfix des TS3 Frameworks </li>
+
+<li> Codebereinigung </li>
+</ul>
+
+<br>
+
+<b> <u> WICHTIG </u> </b>
+
+<b> BITTE DIE NEUE <u> setting.php </u> BENUTZEN ! </b>
---
+++ assets/js/page_backup_repository.js
@@ -0,0 +1,93 @@
+//======================================================================================================================
+// Select backup storage type
+//======================================================================================================================
+
+    $('select[name="storage_type"]').on('change', function() {
+        var $this = $(this);
+
+        var prefix = 'app-storage-settings-';
+        var allElements = $('div[id^="' + prefix + '"]');
+        var selectedElement = $('#' + prefix + $this.val());
+
+        allElements.hide();
+        allElements.find('input[required]').addClass('app-toggle-required').prop('required', false);
+
+        selectedElement.show(200);
+        selectedElement.find('.app-toggle-required').prop('required', true);
+    }).trigger('change');
+
+//======================================================================================================================
+// SFTP
+//======================================================================================================================
+
+    // Common DOM elements.
+    var $sftpPublicKey = $('input[name="sftp_public_key"]');
+    var $sftpPrivateKey = $('input[name="sftp_private_key"]');
+    var $sftpKeyExists = $('#app-sftp-key-exists');
+    var $sftpKeyNotExists = $('#app-sftp-key-not-exists');
+
+    // Select auth method.
+    $('select[name="sftp_auth_method"]').on('change', function() {
+        if ($('select[name="storage_type"]').val() === 'sftp')
+        {
+            var $this = $(this);
+
+            var prefix = 'app-sftp-auth-';
+            var allElements = $('div[id^="' + prefix + '"]');
+            var selectedElement = $('#' + prefix + $this.val());
+
+            allElements.hide();
+            allElements.find('input[required]').addClass('app-toggle-required').prop('required', false);
+
+            selectedElement.show(200);
+            selectedElement.find('.app-toggle-required').prop('required', true);
+        }
+    }).trigger('change');
+
+    // Show public key or 'create public key' button.
+    $sftpPublicKey.on('change', function() {
+        var $this = $(this);
+
+        $sftpKeyExists.hide();
+        $sftpKeyNotExists.hide();
+
+        $this.val() === '' ? $sftpKeyNotExists.show(200) : $sftpKeyExists.show(200);
+    }).trigger('change');
+
+    // Create key pair.
+    $('#app-sftp-create-key').on('click', function() {
+        var $this = $(this);
+
+        ajax({
+            action: 'generate_key_pair',
+            success: function(data)
+            {
+                if (data.error === false)
+                {
+                    $sftpPrivateKey.val(data.private_key);
+                    // Wihtout trigger, on change won't fire on hidden elements.
+                    $sftpPublicKey.val(data.public_key).trigger('change');
+                }
+                else
+                {
+                    $sftpPrivateKey.val('');
+                    $sftpPublicKey.val('ERROR');
+                }
+            },
+            beforeSend: function()
+            {
+                $this.addClass('is-loading');
+            },
+            complete: function()
+            {
+                $this.removeClass('is-loading');
+            }
+        });
+    });
+
+    // Remove key pair.
+    $('#app-sftp-remove-key').on('click', function() {
+        $sftpPrivateKey.val('');
+        // Wihtout trigger, on change won't fire on hidden elements.
+        $sftpPublicKey.val('').trigger('change');
+    });
